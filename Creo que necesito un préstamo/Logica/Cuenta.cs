1dc095958be094c8ad1bbd8b69027e4bac3269f1
matiasnarrua <47338063+matiasnarrua@users.noreply.github.com>
2022-03-28 22:00:51 -0300

    Add files via upload

---
+++ Creo que necesito un préstamo/Logica/Cuenta.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Logica
+{
+    public class Cuenta
+    {
+        public string titular;
+        public float cantidad;
+
+        public Cuenta(string titular, float cantidad)
+        {
+            this.titular = titular;
+            this.cantidad = cantidad;
+        }
+        public string GetTitular()
+        {
+            return this.titular;
+        }
+        public void SetTitular(string value)
+        {
+            if (!string.IsNullOrEmpty(value))
+            {
+                this.titular = value;
+            }
+        }
+
+        public string Mostrar() 
+        {
+            return $"La cuenta del titular {this.titular} Tiene ${this.cantidad} en la cuenta";
+        }
+
+        public void Retirar(float value)
+        {
+             this.cantidad -= value; 
+        }
+
+        public void Ingresar(float value)
+        {
+            this.cantidad += value;
+        }
+
+
+
+
+    }
+}
